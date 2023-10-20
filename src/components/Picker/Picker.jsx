ee9912dae18a021cb34abb14a7227dbcef3bdf08
amezir <amezirmessaoud.pro@gmail.com>
2023-10-20 17:01:03 +0200

    add class active

--- src/components/Picker/Picker.jsx
+++ src/components/Picker/Picker.jsx
@@ -1,19 +1,25 @@
 import style from "./Picker.module.scss";
 import Scene from "../../webgl/Scene";
+import React, { useState } from "react"; // Importer useState depuis React
 
 const Picker = () => {
+    const [activeIndex, setActiveIndex] = useState(null); // État pour suivre l'élément actif
 
     const pickVisualizer = (index) => {
         Scene.changeVisualizer(index);
+
+        // Mettre à jour l'élément actif lorsque vous changez de sélection
+        setActiveIndex(index);
     }
 
     return <div className={style.picker}>
-        <div onClick={() => pickVisualizer(0)}>Cube</div>
-        <div onClick={() => pickVisualizer(1)}>Line</div>
-        <div onClick={() => pickVisualizer(2)}>Logo IUT</div>
-        <div onClick={() => pickVisualizer(3)}>Board</div>
+        <div onClick={() => pickVisualizer(0)} className={activeIndex === 0 ? style.active : ""}>Cube</div>
+        <div onClick={() => pickVisualizer(1)} className={activeIndex === 1 ? style.active : ""}>Line</div>
+        <div onClick={() => pickVisualizer(2)} className={activeIndex === 2 ? style.active : ""}>Logo IUT</div>
+        <div onClick={() => pickVisualizer(3)} className={activeIndex === 3 ? style.active : ""}>Board</div>
     </div>
 };
 
 export default Picker;
+
 
